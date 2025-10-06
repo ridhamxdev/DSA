d09e67da455c5c5c5f6fa42b47bf82c030158a8c
Ridham Goyal <ridhamgoyal3@gmail.com>
2025-10-06 17:06:39 +0530

    Insertion in LL

--- LinkedList/first.cpp
+++ LinkedList/first.cpp
@@ -31,6 +31,48 @@ int LengthLL(Node *head){
     }
     return count;
 }
+
+void insertAtHead(Node* &head,Node* &tail,int data){
+    Node* temp=new Node(data);
+    temp->next=head;
+    head=temp;
+}
+void insertAtTail(Node* &head,Node* &tail,int data){
+    Node* temp=new Node(data);
+    tail->next=temp;
+    tail=temp;
+}
+void insertAtPosition(Node* &head,Node* &tail,int pos,int data){
+    if(pos<1){
+        cout<<"Invalid Position"<<endl;
+        return;
+    }
+    else if(pos==1){
+        insertAtHead(head,tail,pos);
+        return;
+    }
+    else if(pos>LengthLL(head)){
+        insertAtTail(head,tail,pos);
+        return;
+    }
+    else{
+    Node *newnode=new Node(data);
+    Node* curr=NULL;
+    Node* prev=NULL;
+    curr=head;
+    while(pos!=1){
+        pos--;
+        prev=curr;
+        curr=curr->next;
+    }
+    prev->next=newnode;
+    newnode->next=curr;
+    if(curr==NULL){
+        tail=newnode;
+    }
+}
+
+}
 int main(){
     Node *first=new Node(10);
     Node *second=new Node(20);
@@ -50,5 +92,17 @@ int main(){
 
     int x=LengthLL(head);
     cout<<"Length of Linked List is "<<x<<endl;
+
+
+    Node *tail=fifth;
+    insertAtHead(head,tail,17);
+    printLL(head);
+
+    insertAtTail(head,tail,60);
+    printLL(head);
+
+    insertAtPosition(head,tail,3,25);
+    printLL(head);
+
 return 0;
 }
